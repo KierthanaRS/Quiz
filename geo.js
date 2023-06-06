3257a9d4db315293fa27b15bcd2ac38e031f5901
Kierthana <77330790+KierthanaRS@users.noreply.github.com>
2023-06-06 17:58:28 +0530

    Add files via upload

---
+++ geo.js
@@ -0,0 +1,137 @@
+function india(){
+  const quizData = [
+    {
+      question: "Which state in India is known as the 'Land of Spices'?",
+      a: "Kerala",
+      b: "Karnataka",
+      c: "Tamil Nadu",
+      d: "Andhra Pradesh",
+      correct: "a",
+    },
+    {
+      question: "Which Indian state is famous for the Ajanta and Ellora Caves?",
+      a: "Kerala",
+      b: "Maharashtra",
+      c: "Karnataka",
+      d: "Uttar Pradesh",
+      correct: "b",
+    },
+    {
+      question: "The Tropic of Cancer does not pass through which of the following states in India?",
+      a: "Gujarat",
+      b: "Rajasthan",
+      c: "Madhya Pradesh",
+      d: "West Bengal",
+      correct: "b",
+    },
+    {
+      question: "Which state in India is known as the 'Land of High Passes'?",
+      a: "Jammu and Kashmir",
+      b: "Himachal Pradesh",
+      c: "Uttarakhand",
+      d: "Sikkim",
+      correct: "a",
+    },
+    {
+      question: "Which state in India is famous for the Meenakshi Temple?",
+      a: "Karnataka",
+      b: "Andhra Pradesh",
+      c: "Tamil Nadu",
+      d: "Telangana",
+      correct: "c",
+    },
+  ];
+  
+return quizData
+}
+function world(){
+  const quizData = [
+    {
+      question: "What is the capital city of Australia?",
+      a: "Sydney",
+      b: "Melbourne",
+      c: "Canberra",
+      d: "Perth",
+      correct: "c",
+    },
+    {
+      question: "Which country is known as the 'Land of the Rising Sun'?",
+      a: "China",
+      b: "Japan",
+      c: "South Korea",
+      d: "Thailand",
+      correct: "b",
+    },
+    {
+      question: "Which is the largest country by area in the world?",
+      a: "United States",
+      b: "Canada",
+      c: "Russia",
+      d: "China",
+      correct: "c",
+    },
+    {
+      question: "What is the official language of Brazil?",
+      a: "Portuguese",
+      b: "Spanish",
+      c: "English",
+      d: "French",
+      correct: "a",
+    },
+    {
+      question: "Which African country is known as the 'Pearl of Africa'?",
+      a: "Kenya",
+      b: "Ethiopia",
+      c: "Uganda",
+      d: "South Africa",
+      correct: "c",
+    },
+  ];
+ return quizData;  
+}
+function natural(){
+  const quizData = [
+    {
+      question: "Which natural wonder is known as 'The Smoke That Thunders'?",
+      a: "Iguazu Falls",
+      b: "Victoria Falls",
+      c: "Niagara Falls",
+      d: "Angel Falls",
+      correct: "b",
+    },
+    {
+      question: "What is the largest coral reef system in the world?",
+      a: "Great Barrier Reef",
+      b: "Maldives Barrier Reef",
+      c: "Belize Barrier Reef",
+      d: "Red Sea Coral Reef",
+      correct: "a",
+    },
+    {
+      question: "Which natural wonder is located in the U.S. state of Arizona?",
+      a: "Mount Everest",
+      b: "Machu Picchu",
+      c: "Grand Canyon",
+      d: "Great Wall of China",
+      correct: "c",
+    },
+    {
+      question: "What is the name of the tallest waterfall in the world?",
+      a: "Niagara Falls",
+      b: "Angel Falls",
+      c: "Victoria Falls",
+      d: "Iguazu Falls",
+      correct: "b",
+    },
+    {
+      question: "Which mountains are the youngest fold mountain range in the world?",
+      a: "Andes",
+      b: "Alps",
+      c: "Himalayas",
+      d: "Rocky Mountains",
+      correct: "c",
+    },
+  ];
+  
+  return quizData;
+}
